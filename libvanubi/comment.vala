vanubi/vanubi

commit 90b62612cd8c6a06a6f1e6ed62c1338b4a55f423

fix commenting crash

libvanubi/comment.vala
@@ -47,36 +47,57 @@ namespace Vanubi {
 			}
 		}
 
+		/* Return the total lines to be processed skipping empty lines */
+		private int find_tot_lines(int start_line, int end_line) {
+			int count = 0;
+			for (var i=start_line; i<=end_line; i++) {
+				if (buf.empty_line (i)) {
+					continue;
+				}
+				count++;
+			}
+			return count;
+		}
+
 		public virtual void toggle_comment (BufferIter start_iter, BufferIter end_iter) {
 			var start_line = start_iter.line;
 			var end_line = end_iter.line;
-			var tot_lines = (end_line - start_line) + 1;
-			if (tot_lines > 1 && end_iter.line_offset == 0) {
-				end_line--;
-				tot_lines--;
+			var tot_lines = find_tot_lines (start_line, end_line);
+
+			debug ("start-line=%d end-line=%d tot-line=%d",
+			       start_line, end_line, tot_lines);
+
+			if (tot_lines == 0) {
+				return;
 			}
+
 			if (tot_lines < 0) {
 				/* Invalid region */
-				warning ("Invalid comment region [%d]", tot_lines);
+				warning ("Invalid comment region [tot-lines=%d]", tot_lines);
 				return;
-			} else if (tot_lines == 1) { /* Commenting single line */
+			}
+
+			if (tot_lines == 1) { /* Processing single line */
 				find_common_offset (start_line, start_line + 1);
 				if (is_line_commented (start_line)) {
 					decomment_line (start_line);
 				} else {
 					comment_line (start_line);
 				}
-			} else { /* Commenting region */
+			} else { /* Processing region */
 				var commented_lines = count_commented_lines (start_line, end_line);
+				debug ("commented lines: %d", commented_lines);
 				if (commented_lines == tot_lines) {
 					/* Decomment all */
 					for (var i=start_line; i<=end_line; i++) {
 						decomment_line (i);
+						debug ("decommenting line %d", i);
 					}
 				} else { /* Comment all and escape already commented lines */
 					find_common_offset (start_line, end_line);
 					for (var i=start_line; i<=end_line; i++) {
 						comment_line (i);
+						debug ("commenting line %d common-offset=%d", i, common_offset);
 					}
 				}
 			}
@@ -257,11 +278,17 @@ namespace Vanubi {
 		}
 
 		protected override void comment_line (int line) {
+			if (buf.empty_line (line)) {
+				return;
+			}
 			var iter = buf.line_at_char (line, common_offset);
 			buf.insert (iter, "# ");
 		}
 
 		protected override void decomment_line (int line) {
+			if (buf.empty_line (line)) {
+				return;
+			}
 			if (is_line_commented (line)) {
 				var iter = buf.line_start (line);
 				iter.forward_spaces ();
@@ -293,11 +320,17 @@ namespace Vanubi {
 		}
 
 		protected override void comment_line (int line) {
+			if (buf.empty_line (line)) {
+				return;
+			}
 			var iter = buf.line_at_char (line, common_offset);
 			buf.insert (iter, "; ");
 		}
 
 		protected override void decomment_line (int line) {
+			if (buf.empty_line (line)) {
+				return;
+			}
 			if (is_line_commented (line)) {
 				var iter = buf.line_start (line);
 				iter.forward_spaces ();
@@ -330,11 +363,17 @@ namespace Vanubi {
 		}
 
 		protected override void comment_line (int line) {
+			if (buf.empty_line (line)) {
+				return;
+			}
 			var iter = buf.line_at_char (line, common_offset);
 			buf.insert (iter, "-- ");
 		}
 
 		protected override void decomment_line (int line) {
+			if (buf.empty_line (line)) {
+				return;
+			}
 			if (is_line_commented (line)) {
 				var iter = buf.line_start (line);
 				iter.forward_spaces ();

gui/manager.vala
@@ -2268,11 +2268,11 @@ namespace Vanubi.UI {
 				TextIter start, end;
 				ed.view.buffer.get_iter_at_offset (out start, selection_start);
 				ed.view.buffer.get_iter_at_offset (out end, selection_end);
-				
+
 				var iter_start = vbuf.line_at_char (start.get_line (),
-													start.get_line_offset ());
+								    start.get_line_offset ());
 				var iter_end = vbuf.line_at_char (end.get_line (),
-												  end.get_line_offset ());
+								  end.get_line_offset ());
 				ed.view.buffer.begin_user_action ();
 				comment_engine.toggle_comment (iter_start, iter_end);
 				ed.view.buffer.end_user_action ();
@@ -2281,7 +2281,7 @@ namespace Vanubi.UI {
 
 		void on_set_theme (Editor editor) {
 			var themes = state.theme_manager.get_themes ();
-			
+
 			var bar = new SimpleCompletionBar<Theme> ((owned) themes);
 			bar.activate.connect (() => {
 					abort (editor);

commit d5ce81a5408803baad8418c312dc092b0c1aaf03

fix regression introduced by commit 90b62612cd8c6a06a6f1e6ed62c1338b4a55f423

libvanubi/comment.vala
@@ -24,7 +24,6 @@ namespace Vanubi {
 		protected int common_offset;
 
 		protected abstract bool is_line_commented (int line);
-		protected abstract int count_commented_lines (int start_line, int end_line);
 		protected abstract void comment_line (int line);
 		protected abstract void decomment_line (int line);
 
@@ -47,14 +46,12 @@ namespace Vanubi {
 			}
 		}
 
-		/* Return the total lines to be processed skipping empty lines */
-		private int find_tot_lines(int start_line, int end_line) {
-			int count = 0;
+		private int count_commented_lines (int start_line, int end_line) {
+			var count = 0;
 			for (var i=start_line; i<=end_line; i++) {
-				if (buf.empty_line (i)) {
-					continue;
+				if (is_line_commented (i) || buf.empty_line (i)) {
+					count++;
 				}
-				count++;
 			}
 			return count;
 		}
@@ -62,7 +59,7 @@ namespace Vanubi {
 		public virtual void toggle_comment (BufferIter start_iter, BufferIter end_iter) {
 			var start_line = start_iter.line;
 			var end_line = end_iter.line;
-			var tot_lines = find_tot_lines (start_line, end_line);
+			var tot_lines = (end_line - start_line) + 1;
 
 			debug ("start-line=%d end-line=%d tot-line=%d",
 			       start_line, end_line, tot_lines);
@@ -111,16 +108,6 @@ namespace Vanubi {
 
 		protected override bool is_line_commented (int line) {
 			return /\s*\/\*\s?.+\s?\*\//.match(buf.line_text (line));
-		}
-
-		protected override int count_commented_lines (int start_line, int end_line) {
-			var count = 0;
-			for (var i=start_line; i<=end_line; i++) {
-				if (is_line_commented (i) || buf.empty_line(i)) {
-					count++;
-				}
-			}
-			return count;
 		}
 
 		protected void escape_line (int line) {
@@ -270,16 +257,6 @@ namespace Vanubi {
 			return /\s*#\s?.*/.match(buf.line_text (line));
 		}
 
-		protected override int count_commented_lines (int start_line, int end_line) {
-			var count = 0;
-			for (var i=start_line; i<=end_line; i++) {
-				if (is_line_commented (i)) {
-					count++;
-				}
-			}
-			return count;
-		}
-
 		protected override void comment_line (int line) {
 			if (buf.empty_line (line)) {
 				return;
@@ -311,16 +288,6 @@ namespace Vanubi {
 
 		protected override bool is_line_commented (int line) {
 			return /\s*;\s?.*/.match(buf.line_text (line));
-		}
-
-		protected override int count_commented_lines (int start_line, int end_line) {
-			var count = 0;
-			for (var i=start_line; i<=end_line; i++) {
-				if (is_line_commented (i)) {
-					count++;
-				}
-			}
-			return count;
 		}
 
 		protected override void comment_line (int line) {
@@ -357,16 +324,6 @@ namespace Vanubi {
 			return /\s*--\s?.*/.match(buf.line_text (line));
 		}
 
-		protected override int count_commented_lines (int start_line, int end_line) {
-			var count = 0;
-			for (var i=start_line; i<=end_line; i++) {
-				if (is_line_commented (i)) {
-					count++;
-				}
-			}
-			return count;
-		}
-
 		protected override void comment_line (int line) {
 			if (buf.empty_line (line)) {
 				return;
@@ -399,16 +356,6 @@ namespace Vanubi {
 
 		protected override bool is_line_commented (int line) {
 			return /^<!--.*-->$/.match (buf.line_text (line).strip ());
-		}
-
-		protected override int count_commented_lines (int start_line, int end_line) {
-			var count = 0;
-			for (var i=start_line; i<=end_line; i++) {
-				if (is_line_commented (i) || buf.empty_line(i)) {
-					count++;
-				}
-			}
-			return count;
 		}
 
 		protected void escape_line (int line) {